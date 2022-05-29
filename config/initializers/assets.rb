dcfd69da3cebe900a786c4bfa2b4575d5c943738
pauvasquezh <33466913+pauvasquezh@users.noreply.github.com>
2022-05-29 03:40:38 -0400

    add social media icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,11 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
